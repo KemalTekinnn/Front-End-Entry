5bbaf148000716de3274d3d956230f0e465179be
Kemal Tekin <markus.kml@gmail.com>
2021-09-12 21:53:15 +0300

    Add files via upload

--- Udemy/09-Omnifood-Optimizations/js/script.js
+++ Udemy/09-Omnifood-Optimizations/js/script.js
@@ -1,31 +1,34 @@
-console.log("Hello World");
+console.log("Hello world!");
 
-const Mom = "Your Mom";
+const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
+console.log(myName);
 console.log(h1);
-console.log(Mom);
 
 // h1.addEventListener("click", function () {
-//   h1.textContent = Mom;
-//   h1.style.backgroundColor = "Red";
-//   h1.style.padding = "5Rem";
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
 // });
 
-//  Footer Year
+///////////////////////////////////////////////////////////
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
-
 yearEl.textContent = currentYear;
 
-/////////////////////////////////////////////////////////////
-const btNav = document.querySelector(".btn-mobile-nav");
-const header = document.querySelector(".header");
-btNav.addEventListener("click", function () {
-  header.classList.toggle("nav-open");
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
-////////////////////////////////////////////////////////////
-//Smooth scrolling animation
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
 
@@ -33,7 +36,6 @@ allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    console.log(href);
 
     // Scroll back to top
     if (href === "#")
@@ -41,19 +43,22 @@ allLinks.forEach(function (link) {
         top: 0,
         behavior: "smooth",
       });
-    // Scroll links
+
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
-      const section = document.querySelector(href);
-      section.scrollIntoView({ behavior: "smooth" });
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    // mobile nav
+    // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
-      header.classList.toggle("nav-open");
+      headerEl.classList.toggle("nav-open");
   });
 });
-//////////////////////////////////
-//Sticky Nav
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
 const sectionHeroEl = document.querySelector(".section-hero");
 
 const obs = new IntersectionObserver(
@@ -77,6 +82,7 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
