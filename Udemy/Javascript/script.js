18e8a500d87715cb0b3295afe5f06da8dca87873
Kemal Tekin <markus.kml@gmail.com>
2021-10-03 23:44:08 +0300

    Update script.js

--- Udemy/Javascript/script.js
+++ Udemy/Javascript/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Temperature Apmliute Calculator
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 const calcTemAmplitude = function (temps) {
   let max = temps[0];
