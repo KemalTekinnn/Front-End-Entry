021b9a9465238952bf2da8e559e8a41532a5088b
Kemal Tekin <markus.kml@gmail.com>
2021-10-05 21:07:50 +0300

    Add files via upload

--- Udemy/Javascript/Guess My Number/script.js
+++ Udemy/Javascript/Guess My Number/script.js
@@ -11,7 +11,8 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+
 // document.querySelector('.score').textContent = score;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +22,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Error';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      document.querySelector('.highscore').textContent = score;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent =
@@ -40,4 +47,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'LOST';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
 });
