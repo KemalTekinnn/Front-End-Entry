d4831b9ec123b13b0e935490901185a0f82af004
Kemal Tekin <markus.kml@gmail.com>
2021-10-05 22:12:25 +0300

    Add files via upload

--- Udemy/Javascript/script.js
+++ Udemy/Javascript/script.js
@@ -1,22 +1,64 @@
-"use strict";
+'use strict';
 
-// Temperature Apmliute Calculator
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-const calcTemAmplitude = function (temps) {
-  let max = temps[0];
-  let min = temps[0];
+// console.log(document.querySelector('.message').textContent);
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
+// document.querySelector('.message').textContent = 'LOL';
 
-    if (typeof curTemp !== "number") continue;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 22;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
+// document.querySelector('.guess').value = 0;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+// document.querySelector('.score').textContent = score;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Error';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      document.querySelector('.highscore').textContent = score;
+      highScore = score;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Choose lower number please';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'LOSER';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Choose higher number please';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'LOST';
+    }
   }
-  console.log(max, min);
-  return max - min;
-};
+});
 
-const amplitude = calcTemAmplitude(temperatures);
-console.log(amplitude);
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = secretNumber;
+});
