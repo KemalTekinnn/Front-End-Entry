128de7ab75c9635735458947f495317732229873
Kemal Tekin <markus.kml@gmail.com>
2021-10-07 18:47:05 +0300

    Add files via upload

---
+++ Udemy/Javascript/Modal/script.js
@@ -0,0 +1,25 @@
+'use strict';
+console.log('Connected');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  console.log('Close clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  //   console.log(btnsOpenModal[i].textContent);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
