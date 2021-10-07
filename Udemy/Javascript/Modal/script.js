9fdaf553b907a2cf6e5305166fdc20f9c7766d6e
Kemal Tekin <markus.kml@gmail.com>
2021-10-07 21:16:12 +0300

    esc

--- Udemy/Javascript/Modal/script.js
+++ Udemy/Javascript/Modal/script.js
@@ -1,18 +1,16 @@
 'use strict';
-console.log('Connected');
+console.log('Javascript Connected');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button Clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  console.log('Close clicked');
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
@@ -23,3 +21,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('.hidden')) {
+    console.log('Escape Was pressed');
+    closeModal();
+  }
+});
